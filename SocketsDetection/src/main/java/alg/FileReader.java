f7783b72411ba292cb3434387dba6eb44cdafa9e
Marek Klement <marek.klement@bcvsolutions.eu>
2017-11-16 15:23:31 +0100

    This is working

--- SocketsDetection/src/main/java/alg/FileReader.java
+++ SocketsDetection/src/main/java/alg/FileReader.java
@@ -1,6 +1,7 @@
 package alg;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -12,20 +13,31 @@ import java.util.List;
  */
 public class FileReader {
 
+//	private String path;
+//
+//	public FileReader(String path){
+//		this.path = path;
+//	}
+
 	public ConnectionDev read() throws IOException {
+		//
+		int nodes;
+		int edges;
+		List<Integer>[] conns;
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		String[] firstLine = reader.readLine().trim().split("\\s+");
-		int nodes = Integer.parseInt(firstLine[0]);
-		int edges = Integer.parseInt(firstLine[1]);
+//		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
+		String[] firstLine = reader.readLine().split(" ");
+		nodes = Integer.parseInt(firstLine[0]);
+		edges = Integer.parseInt(firstLine[1]);
 		int i;
 		String[] line;
 
-		List[] conns = new List[edges+1];
-		for(i=1;i<=edges;++i){
-			conns[i] = new ArrayList<>();
+		conns = new List[edges + 1];
+		for (i = 1; i <= edges; ++i) {
+			conns[i] = new ArrayList();
 		}
-		for(i=1;i<=edges;++i){
-			line = reader.readLine().trim().split("\\s+");
+		for (i = 1; i <= edges; ++i) {
+			line = reader.readLine().split(" ");
 			int from = Integer.parseInt(line[0]);
 			int to = Integer.parseInt(line[1]);
 			conns[from].add(to);
--- SocketsDetection/src/main/java/alg/Main.java
+++ SocketsDetection/src/main/java/alg/Main.java
@@ -8,8 +8,14 @@ import java.io.IOException;
  */
 public class Main {
 	public static void main(String[] args) throws IOException {
+		//long start = System.currentTimeMillis();
 		FileReader reader = new FileReader();
+//		FileReader reader = new FileReader(args[0]);
 		ConnectionDev dev = reader.read();
 		dev.solve();
+		dev.printResult();
+		//long end = System.currentTimeMillis() - start;
+		//System.out.println();
+		//System.out.println(end);
 	}
 }
